55c6ff4454167ce86eeac29d49471944cebc460f
Nathan Bell <nathanbell09@gmail.com>
2018-10-19 14:43:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,4 +15,23 @@ class TicTacToe
     [6,4,2]
   ]
 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(n)
+    n.to_i - 1
+  end
+
+  def move(u_index, char)
+    @board[u_index] = char
+    @board
+  end
+
+  
+
 end
